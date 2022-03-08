b42aac7cd8d09658fbce84347fc87b1e0f66e9a6
Willian Proni <wifepro@hotmail.com>
2022-03-07 23:46:16 -0300

    Adicionar arquivos de projeto.

---
+++ Senha correta/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Senha_correta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+
+            Console.WriteLine("--Teste de senha--");
+
+            Console.Write("\nDigite a senha: ");
+            senha = Convert.ToInt32(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.Write("\nDigite a senha: ");
+                senha = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Senha correta");
+        }
+    }
+}
